f8a055a37de1ddbb136370629b923f0b6049a2fd
Cameron Wagstaff <cam.wagstaff@gmail.com>
2021-04-15 00:24:46 -0400

    pass mpv args to handler from extension
    Storage and manipulation of user options is now the extension's
    responsibility. Now the exercise of exposing those options begins.

--- app/openMpv.go
+++ app/openMpv.go
@@ -2,31 +2,11 @@ package main
 
 import (
 	"encoding/base64"
+	"fmt"
 	"os"
 	"os/exec"
 	"strings"
 )
-
-/*
-Protocol that should be implemented for all of this mess:
-
-$prefix$path$video$args
-
-$prefix is the string literal "mpv://"
-$path is the base64 encoded location of the user's local mpv executable
-$video is a base64 encoded video url
-$args is zero or more occurrences of $arg
-$arg = $sep$param$sep$value
-$sep is a url-allowable separator character outside of the base64 set
-$param is a base64 encoded mpv argument key, not including whitespace, "--" or "="
-$value is a base64 encoded mpv argument value. can be empty string to denote no value(equals will be omitted from argument)
-*/
-var mpvPath = "C:\\mpv\\mpv.exe"
-var mpvArgs = []string{
-	"--no-border",
-	"--ytdl-format=bestvideo+bestaudio/best",
-	"--autofit=60%",
-}
 
 const g_separator = "_"
 
@@ -35,30 +15,93 @@ func main() {
 		os.Exit(1)
 	}
 
-	mpvArgs, err := parse(os.Args[1])
+	command, err := parse(os.Args[1])
 	if err != nil {
 		os.Exit(1)
 	}
 
-	err = exec.Command(mpvPath, mpvArgs...).Start()
+	err = exec.Command(command.Path, command.Args...).Start()
 	if err != nil {
 		os.Exit(1)
 	}
 }
 
-//TODO re-implement following the protocol defined above
-func parse(url string) (args []string, err error) {
+type Command struct {
+	// Local path to mpv executable, including the executable.
+	Path string
+
+	// Command line arguments to pass when calling mpv.
+	Args []string
+}
+
+/*
+returns a command that can be sent to mpv
+
+
+Protocol for mpv stuff:
+
+message: $prefix$path$sep$video$args
+
+$prefix is the string literal "mpv://"
+
+$path is the base64 encoded location of the user's local mpv executable
+
+$video is a base64 encoded video url
+
+$args is zero or more occurrences of $arg
+
+$arg = $sep$param$sep$value
+
+$sep is a url-allowable separator character outside of the base64 set
+
+$param is a base64 encoded mpv argument key, not including whitespace, "--" or "="
+
+$value is a base64 encoded mpv argument value. can be empty string to denote no value(equals will be omitted from argument)
+*/
+func parse(url string) (mpv Command, err error) {
 	url = strings.TrimPrefix(url, "mpv://")
 
 	parts := strings.Split(url, g_separator)
 
-	videoUrl, err := base64.StdEncoding.DecodeString(parts[0])
+	mpv.Path, err = decode(parts[0])
 	if err != nil {
 		return
 	}
-	args = append(args, string(videoUrl))
 
-	args = append(args, mpvArgs...)
+	videoUrl, err := decode(parts[1])
+	if err != nil {
+		return
+	}
+	mpv.Args = append(mpv.Args, videoUrl)
+
+	for i := 2; i < len(parts); i += 2 {
+		argName, inErr := decode(parts[i])
+		if inErr != nil {
+			// I'm okay with discarding broken arguments
+			continue
+		}
+
+		if len(parts[i+1]) == 0 {
+			mpv.Args = append(mpv.Args, fmt.Sprintf("--%s", argName))
+			continue
+		}
+
+		argValue, inErr := decode(parts[i+1])
+		if inErr != nil {
+			continue
+		}
+		mpv.Args = append(mpv.Args, fmt.Sprintf("--%s=%s", argName, argValue))
+	}
 
 	return
+}
+
+// returns a string decoded from the base64 string provided
+func decode(encoded string) (decoded string, err error) {
+	temp, err := base64.StdEncoding.DecodeString(encoded)
+	if err != nil {
+		return
+	}
+
+	return string(temp), nil
 }
--- main.js
+++ main.js
@@ -1,6 +1,42 @@
+let settings = {
+    mpvPath: 'C:\\mpv\\mpv.exe',
+    mpvArgs: [
+        {
+            'name': 'no-border',
+            'value': null
+        },
+        {
+            'name': 'ytdl-format',
+            'value': 'bestvideo+bestaudio/best'
+        },
+        {
+            'name': 'autofit',
+            'value': '60%'
+        }
+    ]
+}
+
+// Store user options in encoded format
+let encoded = {
+    prefix: '',
+    suffix: ''
+}
+
+function encodeSettings(settings) {
+    encoded.prefix = 'mpv://' + btoa(settings.mpvPath) + '_'
+    encoded.suffix = settings.mpvArgs.reduce((acc, cur) => {
+        acc += '_' + btoa(cur.name) + '_'
+
+        if (cur.value !== null) {
+            acc += btoa(cur.value)
+        }
+
+        return acc
+    }, "")
+}
 
 function playLink(info, tab) {
-    let mpvUrl = 'mpv://' + btoa(info.linkUrl)
+    let mpvUrl = encoded.prefix + btoa(info.linkUrl) + encoded.suffix
 
     // Chrome falls back to the OS to handle unknown(to it) protocols prior to navigating.
     // Therefore we can just redirect the current tab to this url without fear of disrupting it.
@@ -13,6 +49,7 @@ function playLink(info, tab) {
 chrome.contextMenus.onClicked.addListener(playLink)
 
 chrome.runtime.onInstalled.addListener(function () {
+    encodeSettings(settings)
     chrome.contextMenus.create({
         title: 'Open in MPV',
         type: 'normal',
